bc07f6e4b792660b23f7062d1ef83e9d9c9ba121
aleksa-petrovic10 <9leksa2@gmail.com>
2021-02-26 16:40:25 +0100

    first commit

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,21 @@
+describe("Login test", () => {
+    it('Visit gallery page', () => {
+        cy.visit('/') 
+    })
+    it("Click on login button", () => {
+        cy.get("a[href='/login']").click()
+    })
+    it ('Login with valid credentials', () => {
+        cy.get('#email').type('pr@gmail.com')
+        cy.get('#password').type('11111111')
+        cy.get('button').click()
+    })
+    it("Visit create gallery page", () => {
+        cy.get('a[href="/create"]').click()
+        cy.get('#title').type('luda galerija');
+        cy.get('#description').type('stvarno luda')
+    })
+    it ('Logout', () => {
+        cy.get('.ml-auto > :nth-child(3) > .nav-link').click()
+    })
+})
